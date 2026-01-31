d4e5cd4c3ce9a65f2ab3cf2c28a79a250b1755c7
premkumaruniverse <premkumaruniverse0@gmail.com>
2026-02-01 02:10:14 +0530

    calling feature

--- BE/app/main.py
+++ BE/app/main.py
@@ -18,11 +18,12 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:8000",
+    "*" # Allow all origins for deployment
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"], # Allow all origins for deployment
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
