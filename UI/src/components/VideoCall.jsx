b865702cdd848d55028b78ef3cff86f96b0684a1
prembyldwealth <prem@byldwealth.in>
2026-03-08 19:03:44 +0530

    feat: add public TURN server support for cross-network video calls

--- UI/src/components/VideoCall.jsx
+++ UI/src/components/VideoCall.jsx
@@ -26,14 +26,27 @@ const VideoCall = ({ onEndCall, incomingSignal, sendSignal }) => {
                 { urls: 'stun:stun.l.google.com:19302' },
                 { urls: 'stun:stun1.l.google.com:19302' },
                 { urls: 'stun:stun2.l.google.com:19302' },
-                { urls: 'stun:stun3.l.google.com:19302' },
-                { urls: 'stun:stun4.l.google.com:19302' },
-                { urls: 'stun:global.stun.twilio.com:3478' }
+                { urls: 'stun:global.stun.twilio.com:3478' },
+                // OpenRelay Project Free TURN Servers for testing
+                {
+                    urls: "turn:openrelay.metered.ca:80",
+                    username: "openrelayproject",
+                    credential: "openrelayproject",
+                },
+                {
+                    urls: "turn:openrelay.metered.ca:443",
+                    username: "openrelayproject",
+                    credential: "openrelayproject",
+                },
+                {
+                    urls: "turn:openrelay.metered.ca:443?transport=tcp",
+                    username: "openrelayproject",
+                    credential: "openrelayproject",
+                }
             ],
             iceCandidatePoolSize: 10,
             bundlePolicy: 'max-bundle',
-            rtcpMuxPolicy: 'require',
-            iceTransportPolicy: 'all'
+            rtcpMuxPolicy: 'require'
         });
 
         pc.onicecandidate = (event) => {
